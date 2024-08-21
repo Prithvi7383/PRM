2f3a45a258cbefecd5a1eae0252a66329cec280a
Prithvi7383 <pruathviraj11@gmail.com>
2024-08-22 00:06:10 +0530

    first commit

---
+++ product/src/test/java/com/product/productRetailManagement/ProductRetailManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.productRetailManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRetailManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
